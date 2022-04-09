d184c68c1b58a2934b48490bf424d9d6d5ae67a7
Vladimiros Makris <82274091+vladimak97@users.noreply.github.com>
2022-04-09 13:28:56 +0200

    Update and rename PrivateController.java to PrivateApp.java

--- spring/src/main/java/com/example/demo/PrivateApp.java
+++ spring/src/main/java/com/example/demo/PrivateApp.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class PrivateController {
+public class PrivateApp {
 
     @RequestMapping("/")
+    @ResponseBody
     public String index() {
-        return "index";
-}
+        return "Good morning!";
+    }
