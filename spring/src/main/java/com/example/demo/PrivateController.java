3ff1d24bd475ae316e72cddcfb3ccf895edb15c5
Vladimiros Makris <82274091+vladimak97@users.noreply.github.com>
2022-04-09 13:27:21 +0200

    Update PrivateController.java

--- spring/src/main/java/com/example/demo/PrivateController.java
+++ spring/src/main/java/com/example/demo/PrivateController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PrivateApp {
+public class PrivateController {
 
     @RequestMapping("/")
     public String index() {
